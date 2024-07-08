6bef527624a5ccab42711155b474203489df4221
Asbel Jhonatan Apaza Romero <asbel.elmec@gmail.com>
2024-07-08 14:30:30 -0400

    [add] post images

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 import { z } from "astro/zod";
-import { defineCollection } from "astro:content";
+import { defineCollection, reference } from "astro:content";
 
 // import {z} from 'astro:content'
 const blogCollection = defineCollection({
@@ -14,14 +14,22 @@ const blogCollection = defineCollection({
     }),
 
     // Relacion
-    author: z.string(),
+    author: reference('author'),
 
     // Relacion
     tags: z.array(z.string()),
   }),
 });
 
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({image}) => z.object({
+    name: z.string(),
+    avatar: image(),
+  }),
+});
 
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 }
