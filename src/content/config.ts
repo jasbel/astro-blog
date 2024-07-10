4803bd4836dfa3cc28194c84e046e4ca986dc959
jasbel <asbel.elmec@gmail.com>
2024-07-10 18:47:29 -0400

    changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,32 +4,42 @@ import { defineCollection, reference } from "astro:content";
 // import {z} from 'astro:content'
 const blogCollection = defineCollection({
   type: "content",
-  schema: ({image}) => z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    // image: z.string(),
-    image: image().refine(img => img.width < 1200, {
-      message: 'Image should be lower than 1200px'
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      // image: z.string(),
+      image: image().refine((img) => img.width < 1200, {
+        message: "Image should be lower than 1200px",
+      }),
+
+      // Relacion
+      author: reference("author"),
+
+      // Relacion
+      tags: z.array(z.string()),
+
+      // Boolean
+      isDraft: z.boolean().default(false),
     }),
-
-    // Relacion
-    author: reference('author'),
-
-    // Relacion
-    tags: z.array(z.string()),
-  }),
 });
 
 const authorCollection = defineCollection({
   type: "data",
-  schema: ({image}) => z.object({
-    name: z.string(),
-    avatar: image(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+      twitter: z.string(),
+      linkedIn: z.string(),
+      github: z.string(),
+      bio: z.string(),
+      subtitle: z.string(),
+    }),
 });
 
 export const collections = {
   blog: blogCollection,
   author: authorCollection,
-}
+};
